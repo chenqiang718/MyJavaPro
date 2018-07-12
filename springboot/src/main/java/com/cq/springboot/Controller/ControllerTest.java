1cfb525829a86d369949fcb32cb0ac8ebabcee8b
chenqiang <1178466767@qq.com>
2018-07-12 19:01:15 +0800

    完善

--- springboot/src/main/java/com/cq/springboot/Controller/ControllerTest.java
+++ springboot/src/main/java/com/cq/springboot/Controller/ControllerTest.java
@@ -1,12 +1,18 @@
 package com.cq.springboot.Controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.UnsupportedEncodingException;
+
 @RestController
 public class ControllerTest {
+    @Value("${student.name}")
+    private String name;
     @RequestMapping("/Hello")
-    public String SayHello(){
-        return "你好";
+    public String SayHello() throws UnsupportedEncodingException {
+        System.out.println(name);
+        return "你好:"+name;
     }
 }
