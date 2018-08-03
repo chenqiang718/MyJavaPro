0b391552a37e15c7128f926a5f1aed75036b6247
chenqiang <1178466767@qq.com>
2018-08-03 18:26:00 +0800

    增加Email功能

---
+++ src/main/java/cq/common/EmailUtils.java
@@ -0,0 +1,66 @@
+package cq.common;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import java.io.UnsupportedEncodingException;
+import java.util.Date;
+import java.util.Properties;
+
+/**
+ * 
+ * 发送邮件工具
+ * 
+ * @author: 尚正平(shangzhengping@adtime.com)
+ * @date: 2017年4月28日 上午10:17:11
+ * @Copyright: 2016 浙江德嘉信息技术有限公司 All Rights Reserved
+ */
+public class EmailUtils {
+
+/*	private static final String SEND_USER = "yq_service@163.com";
+	private static final String SEND_PWD = "8R80znN52017";*/
+
+/*	private static final String SEND_USER = "m13588014127@163.com";
+	private static final String SEND_PWD = "a64500507z";*/
+
+/*	private static final String SEND_USER = "chenqiang@adtime.com";
+	private static final String SEND_PWD = "A64500507z";*/
+
+	private static final String SEND_USER = "chenqiang718@sina.com";
+	private static final String SEND_PWD = "18232582";
+
+
+	public static final void sendEmail(String[] email, String title, String context) throws UnsupportedEncodingException, MessagingException {
+		String VALUE_SMTP = "smtp.sina.com.cn";
+		Properties props = System.getProperties();
+		props.setProperty("mail.smtp.host", VALUE_SMTP);
+		props.setProperty("mail.smtp.port", "25");
+		props.put("mail.smtp.auth", "true");
+		Session session = Session.getDefaultInstance(props, new Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(SEND_USER, SEND_PWD);
+			}
+		});
+		Message msg = new MimeMessage(session);
+		msg.setFrom(new InternetAddress(SEND_USER, title));
+		InternetAddress[] tmp = new InternetAddress[email.length];
+		for (int i = 0; i < email.length; i++) {
+			tmp[i] = new InternetAddress(email[i]);
+		}
+		msg.setRecipients(Message.RecipientType.TO, tmp);
+		msg.setSubject(title);
+		msg.setSentDate(new Date());
+		MimeBodyPart mbp1 = new MimeBodyPart();
+		mbp1.setContent(context, "text/html;charset=gbk");
+		MimeMultipart mp = new MimeMultipart();
+		mp.addBodyPart(mbp1);
+		msg.setContent(mp);
+		msg.addRecipients(MimeMessage.RecipientType.CC, InternetAddress.parse(SEND_USER));
+		Transport transport = session.getTransport("smtp");
+		transport.connect(VALUE_SMTP, SEND_USER, SEND_PWD);
+		Transport.send(msg);
+		transport.close();
+	}
+}
--- src/main/java/cq/manager/SystemManager.java
+++ src/main/java/cq/manager/SystemManager.java
@@ -1,8 +1,10 @@
 package cq.manager;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import cq.Model.Seed;
 import cq.Service.SeedService;
+import cq.common.ExcelUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.File;
@@ -14,6 +16,7 @@ public class SystemManager {
     public void init(){
 //        runinsert();
         System.out.println("你好");
+        runTest2();
     }
 
     private void runinsert(){
@@ -52,6 +55,41 @@ public class SystemManager {
                 }
             }
         }
+    }
+
+    private void runTest2(){
+        String file="D:/2018/itfin_url.xlsx";
+        int a=0,b=0,c=0;
+        List<JSONObject> jsonObjectList=null;
+        JSONArray body=new JSONArray();
+        try {
+            jsonObjectList=seedService.readExcelFile(new File(file));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if(jsonObjectList!=null){
+            a=jsonObjectList.size();
+            for(JSONObject jsonObject:jsonObjectList){
+                String company=jsonObject.getString("1");
+                String url=jsonObject.getString("2");
+                List<Seed> seedList=seedService.selectSeed(url);
+                if(seedList.size()>0){
+                    b++;
+                    jsonObject.put("3", "1");
+                }else {
+                    c++;
+                    jsonObject.put("3", "0");
+                }
+                body.add(jsonObject);
+            }
+        }
+        JSONArray head=new JSONArray();
+        String[] arrhead={"0","1","2","3"};
+        for(String str:arrhead){
+            head.add(str);
+        }
+        //ExcelUtils.outFile(ExcelUtils.expExcel("itfin_url", head , body), "D:/2018/itfin_url2.xlsx");
+        System.out.println("总大小：【"+a+"】     存在数目:【"+b+"】    不存在数目:【"+c+"】");
     }
 
     public void monitor(){}
